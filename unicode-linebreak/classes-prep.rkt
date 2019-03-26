6dbbf638480454e3ae3f19db0766da930c9e55e0
Matthew Butterick <mb@mbtype.com>
2019-03-25 21:59:50 -0700

    start pattern prep

---
+++ unicode-linebreak/classes-prep.rkt
@@ -0,0 +1,27 @@
+#lang debug racket/base
+
+(module+ reader
+  (require syntax/strip-context racket/match racket/string)
+  (provide (rename-out [rs read-syntax]))
+  (define (rs name ip)
+    (define lines
+      (for*/list ([line (in-lines ip)]
+                  [str (in-value (string-trim (string-trim line #px"#.*" #:left? #false)))]
+                  #:when (non-empty-string? str))
+        (match-define (list maybe-range tag) (string-split str ";"))
+        (match-define (list left right)
+          (map (λ (str) (string->number str 16))
+               (match (string-split maybe-range "..")
+                 [(list left right) (list left right)]
+                 [(list left) (list left left)])))
+        (list left right (string->symbol tag))))
+    (strip-context
+     (with-syntax ([(LINE ...) lines])
+       #'(module _ racket/base
+           'LINE ...)))))
+
+(provide (rename-out [mb #%module-begin]))
+
+(define-syntax-rule (mb . lines)
+  #`(#%module-begin
+     . lines))
